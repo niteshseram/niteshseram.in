11ef202cda30eda5942e7ce5bf340d62e93ea3b9
S Nitesh Singh <niteshseram@gmail.com>
2021-07-31 19:35:52 +0530

    chore: :wrench: update storybook config and adding a11y addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,8 +5,13 @@ module.exports = {
 	stories: [
 		'../stories/**/*.stories.mdx',
 		'../stories/**/*.stories.@(js|jsx|ts|tsx)',
+		'../components/**/*.stories.@(js|jsx|ts|tsx)',
 	],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-a11y',
+	],
 	webpackFinal: async (config) => {
 		return {
 			...config,
