c5f9769ff5ca4e5875e910b9dc0633686da0229d
S Nitesh Singh <niteshseram@gmail.com>
2021-08-07 15:24:25 +0530

    refactor: :sparkles: create an under construction home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import { Box, Flex, Heading, Link, Text } from '@chakra-ui/react'
-import styles from '../styles/Home.module.css'
+import { Center, Flex, Heading, Link, Text, VStack } from '@chakra-ui/react'
 import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 
 export default function Home() {
 	return (
@@ -13,7 +13,7 @@ export default function Home() {
 			align='center'
 		>
 			<Head>
-				<title>Create Next App</title>
+				<title>Nitesh Seram</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<Flex
@@ -24,68 +24,20 @@ export default function Home() {
 				justify='center'
 				align='center'
 			>
-				<Heading m='0' lineHeight='1.15' fontSize='4rem' textAlign='center'>
-					Welcome to{' '}
-					<Link
-						color='blue.400'
-						textDecor='none'
-						_hover={{ textDecor: 'underline' }}
-						_focus={{ textDecor: 'underline' }}
-						_active={{ textDecor: 'underline' }}
-						href='https://nextjs.org'
-					>
-						Next.js!{' '}
-					</Link>
-				</Heading>
-
-				<Text lineHeight='1.5' fontSize='1.5rem'>
-					Get started by editing{' '}
-					<code className={styles.code}>pages/index.js</code>
-				</Text>
-
-				<Box className={styles.grid}>
-					<a href='https://nextjs.org/docs' className={styles.card}>
-						<Heading>Documentation &rarr;</Heading>
-						<Text>
-							Find in-deTextth information about Next.js features and API.
+				<Center>
+					<VStack textAlign='center' spacing='4'>
+						<Heading as='h2' variant='h2'>
+							This site is currently under construction
+						</Heading>
+						<Text variant='preTitle'>
+							For now you can visit my current{' '}
+							<Link color='purple.600' isExternal href='https://niteshseram.in'>
+								portfolio
+							</Link>
 						</Text>
-					</a>
-
-					<a href='https://nextjs.org/learn' className={styles.card}>
-						<h3>Learn &rarr;</h3>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a
-						href='https://github.com/vercel/next.js/tree/master/examples'
-						className={styles.card}
-					>
-						<h3>Examples &rarr;</h3>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-						className={styles.card}
-					>
-						<h3>Deploy &rarr;</h3>
-						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</Box>
+					</VStack>
+				</Center>
 			</Flex>
-
-			<footer className={styles.footer}>
-				<a
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Powered by{' '}
-					<img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-				</a>
-			</footer>
 		</Flex>
 	)
 }
