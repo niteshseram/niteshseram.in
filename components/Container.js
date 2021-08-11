7632da83a22bd18bee2750e4ed6d31f6ca3c88a3
S Nitesh Singh <niteshseram@gmail.com>
2021-08-11 16:52:37 +0530

    feat: :sparkles: add footer

--- components/Container.js
+++ components/Container.js
@@ -1,20 +1,27 @@
 import {
 	Box,
+	Center,
 	Flex,
-	Heading,
+	HStack,
+	Icon,
 	IconButton,
 	Link,
+	Text,
 	useColorMode,
+	VStack,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/dist/client/router'
 import Head from 'next/head'
-import NextLink from 'next/link'
 import { IoMoon, IoSunnyOutline } from 'react-icons/io5'
+import useColorModeSwitcher from '../utils/hooks/useColorModeSwitcher'
+import useToggle from '../utils/hooks/useToggle'
+import { github, linkedin, twitter } from '../content/socials'
 import Logo from './assets/logo'
+import StyledLink from './styled'
 
 const Container = ({ children }) => {
 	const router = useRouter()
-
+	const [isOpen, toggleIsOpen] = useToggle()
 	const meta = {
 		title: 'Nitesh Seram - Software Engineer',
 		description:
@@ -46,22 +53,29 @@ const Container = ({ children }) => {
 				<meta name='twitter:description' content={meta.description} />
 				<meta name='twitter:image' content={meta.image} />
 			</Head>
-			<Box as='main' w='100vw' h='100vh'>
-				<NavBar />
-				{children}
-				<Footer />
+			<Box
+				w={{ base: '100vw', lg: '90vw', '2xl': '72vw' }}
+				minH='100vh'
+				m='auto'
+			>
+				<NavBar toggleIsOpen={toggleIsOpen} />
+				<VStack spacing='32rem' id='skip' as='main'>
+					{isOpen ? 'Hello' : children}
+					<Footer />
+				</VStack>
 			</Box>
 		</Box>
 	)
 }
 
-const NavBar = () => {
+const NavBar = ({ isOpen, toggleIsOpen }) => {
 	const { colorMode, toggleColorMode } = useColorMode()
 	return (
-		<Flex p='4' alignItemsc='center' justify='space-between'>
+		<Flex p='4' alignItems='center' justify='space-between'>
+			<MenuButton isOpen={isOpen} toggleIsOpen={toggleIsOpen} />
 			<Logo />
 			<Flex alignItems='center'>
-				<Flex as='ul'>
+				<Flex display={{ base: 'none', lg: 'flex' }} as='ul'>
 					<Item href='/'>Home</Item>
 					<Item href='/about'>About</Item>
 					<Item href='/projects'>Projects</Item>
@@ -86,16 +100,130 @@ const NavBar = () => {
 	)
 }
 
-const Item = ({ children, href }) => (
+const MenuButton = ({ toggleIsOpen, ...props }) => {
+	const [clicked, toggleClicked] = useToggle()
+
+	const handleClick = () => {
+		toggleIsOpen()
+		toggleClicked()
+	}
+	return (
+		<IconButton
+			borderRadius='sm'
+			onClick={handleClick}
+			display={{ base: 'block', lg: 'none' }}
+			w='48px'
+			h='48px'
+			variant='ghost'
+			_hover={{ variant: 'ghost' }}
+			{...props}
+			icon={<MenuIcon clicked={clicked} />}
+		/>
+	)
+}
+
+const MenuIcon = ({ clicked }) => {
+	const { colorDark } = useColorModeSwitcher()
+	return (
+		<Box w='100%' h='100%' position='relative'>
+			<Line
+				left={clicked ? '8px' : '4px'}
+				bg={colorDark}
+				top={clicked ? '22px' : '10px'}
+				transform={clicked ? 'rotate(45deg)' : 'none'}
+				width={clicked ? '32px' : '40px'}
+			/>
+			<Line
+				left={clicked ? '8px' : '4px'}
+				top={clicked ? '22px' : '20px'}
+				transform={clicked ? 'translateX(30px)' : 'none'}
+				bg={clicked ? 'transparent' : colorDark}
+				width={clicked ? '32px' : '26px'}
+			/>
+			<Line
+				left={clicked ? '8px' : '4px'}
+				transform={clicked ? 'rotate(-45deg)' : 'none'}
+				bg={colorDark}
+				bottom={clicked ? '22px' : '14px'}
+				width={clicked ? '32px' : '16px'}
+			/>
+		</Box>
+	)
+}
+
+const Line = ({ ...props }) => (
+	<Box
+		{...props}
+		borderRadius='5px'
+		as='span'
+		position='absolute'
+		height='4px'
+		transition='all 0.4s ease-in-out'
+	/>
+)
+
+const Item = ({ children, href, ...props }) => (
 	<Box as='li' listStyleType='none'>
-		<NextLink href={href}>
-			<Link href={href} mr='1rem' variant='body'>
-				{children}
-			</Link>
-		</NextLink>
+		<StyledLink {...props} href={href}>
+			{children}
+		</StyledLink>
 	</Box>
 )
 
-const Footer = () => <Heading variant='h3'>Footer</Heading>
+const Footer = () => (
+	<VStack
+		borderTop='1px solid'
+		borderColor='neutral.200'
+		h='150px'
+		py='32px'
+		w='80%'
+		spacing={{ base: '16px' }}
+		as='footer'
+	>
+		<HStack as='ul'>
+			<SocialLink icon={github.icon} href={github.href} />
+			<SocialLink
+				icon={linkedin.icon}
+				href={linkedin.href}
+				name={linkedin.name}
+			/>
+			<SocialLink icon={twitter.icon} href={twitter.href} name={twitter.name} />
+		</HStack>
+		<Text align='center'>© Designed &amp; Built by Nitesh Seram</Text>
+	</VStack>
+)
+
+const SocialLink = ({ children, icon, href }) => {
+	const [hover, toggleHover] = useToggle()
+	return (
+		<Center
+			onMouseEnter={toggleHover}
+			onMouseLeave={toggleHover}
+			as='li'
+			listStyleType='none'
+		>
+			<Link
+				variant='subtle'
+				display='flex'
+				alignItems='center'
+				p={2}
+				href={href}
+				isExternal
+			>
+				<Icon
+					transform={hover ? 'translateY(-4px)' : 'none'}
+					transitionProperty='transform'
+					transitionDuration='500ms'
+					mr={{ lg: '1rem' }}
+					ml={{ lg: '1rem' }}
+					boxSize={{ base: '1.5rem', lg: '1.7rem' }}
+					aria-hidden
+					as={icon}
+				/>
+				{children}
+			</Link>
+		</Center>
+	)
+}
 
 export default Container
