f6e256078796d55fe738b2ba9521260bc1be3402
S Nitesh Singh <niteshseram@gmail.com>
2021-08-11 16:50:50 +0530

    feat: :sparkles: add useToggle hook for open and close menu icon

---
+++ utils/hooks/useToggle.js
@@ -0,0 +1,11 @@
+import { useCallback, useState } from 'react'
+
+const useToggle = (initialValue = false) => {
+	const [value, setValue] = useState(initialValue)
+	const toggle = useCallback(() => {
+		setValue((v) => !v)
+	}, [])
+	return [value, toggle]
+}
+
+export default useToggle
