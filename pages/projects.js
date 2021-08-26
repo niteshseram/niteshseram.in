176f43eecb3f11d8da6c390f3c7d31645952a934
S Nitesh Singh <niteshseram@gmail.com>
2021-08-26 18:30:32 +0530

    feat: :sparkles: create projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,20 +1,48 @@
-import { Heading, Text } from '@chakra-ui/react'
+import { Box, Heading, List, Text } from '@chakra-ui/react'
 import Container from '../layout/container'
+import ContentWrapper from '../layout/contentWrapper'
+import projects from '../content/projects'
+import ProjectCard from '../components/projectCard'
 
 const Projects = () => (
 	<Container title='Projects | Nitesh Seram'>
-		<Heading as='h1' variant='h2'>
-			Projects 🎲
-		</Heading>
-		<Text>
-			This page will contain a list of projects I&apos;ve made or have worked
-			on.
-		</Text>
-		<Text>
-			Each project will contain a summary, live link (if applicable) and a link
-			to the project repository.
-		</Text>
+		<ContentWrapper>
+			<Intro />
+		</ContentWrapper>
 	</Container>
+)
+
+const Intro = () => (
+	<Box w={{ base: '100%', lg: '90%' }} as='section' mb='2rem' mt='2rem'>
+		<Heading as='h1' variant='h1'>
+			Projects
+		</Heading>
+		<Text variant='subtitle' mb='4rem' w='100%'>
+			Here are some projects that I&apos;ve recently worked on.
+		</Text>
+		<ProjectList />
+	</Box>
+)
+
+const ProjectList = () => (
+	<List
+		mx='auto'
+		justifyContent='space-between'
+		display={{ base: 'block', '2xl': 'flex' }}
+		flexWrap='wrap'
+	>
+		{projects.map((project) => (
+			<ProjectCard
+				mb={{ base: '2rem', lg: '8rem' }}
+				title={project.title}
+				description={project.description}
+				tools={project.tools}
+				live={project.live}
+				repo={project.repo}
+				key={project.id}
+			/>
+		))}
+	</List>
 )
 
 export default Projects
