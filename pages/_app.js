56a06b95bd27ec950a22bb422a37c69f6ee48441
S Nitesh Singh <niteshseram@gmail.com>
2021-08-27 16:56:33 +0530

    refactor: :recycle: remove unnessary import

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 import customTheme from '../styles/theme'
-import '@fontsource/inter/300.css'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/700.css'
 
 function MyApp({ Component, pageProps }) {
 	return (
