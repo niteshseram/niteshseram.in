4be0fd3424d72d256d98ce4c8b40861d9080a426
S Nitesh Singh <niteshseram@gmail.com>
2021-09-08 18:47:31 +0530

    chore: :mag: adding google site verification code

--- layouts/Container.js
+++ layouts/Container.js
@@ -62,6 +62,10 @@ const Seo = ({ ...customMeta }) => {
 			{meta.date && (
 				<meta property='article:published_time' content={meta.date} />
 			)}
+			<meta
+				name='google-site-verification'
+				content='cTvq0i7fkRjh7wtvWKlnG42rayvxZBES7MzLl8CoC4k'
+			/>
 		</Head>
 	)
 }
