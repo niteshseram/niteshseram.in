795c11b6e423529d8249806167feaed25423ae5c
S Nitesh Singh <niteshseram@gmail.com>
2021-09-22 13:52:46 +0530

    feat: :chart_with_upwards_trend: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,43 @@
-import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import '@/styles/globals.css'
+import { pageview, GA_TRACKING_ID } from '@/lib/analytics'
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter()
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			pageview(url)
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
 	return (
-		<ThemeProvider attribute='class' enableSystem={false}>
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<>
+			<Script
+				strategy='afterInteractive'
+				src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+			/>
+
+			<Script strategy='afterInteractive' id='ga-script'>
+				{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+        `}
+			</Script>
+			<ThemeProvider attribute='class' enableSystem={false}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
 	)
 }
 
