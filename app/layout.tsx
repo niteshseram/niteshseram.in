896e59d45fb572aa220c193eee3ed37b0813eeeb
Nitesh Seram <niteshseram@gmail.com>
2023-05-21 00:06:07 +0530

    Add title, blur image, JSON-LD for blog and customize scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,47 +71,15 @@ export default function RootLayout({
 			<body
 				className={clsx('antialiased bg-light dark:bg-dark', inter.variable)}
 			>
-				<div className='relative'>
-					<svg
-						className='
-						pointer-events-none
-						absolute
-						top-0
-						left-0
-						h-full
-						w-full
-						isolate
-						z-50
-						dark:opacity-100
-						opacity-50
-						dark:mix-blend-soft-light
-						mix-blend-multiply
-					 '
-					>
-						<filter id='noise'>
-							<feTurbulence
-								type='fractalNoise'
-								baseFrequency='0.80'
-								numOctaves='4'
-								stitchTiles='stitch'
-							/>
-						</filter>
-						<rect
-							width='100%'
-							height='100%'
-							filter='url(#noise)'
-						></rect>
-					</svg>
-					<Provider>
-						<Navbar />
-						<div className='max-w-4xl w-[92vw] sm:w-[90vw] mx-auto'>
-							<StickySocial />
-							{children}
-							<Footer />
-							<Analytics />
-						</div>
-					</Provider>
-				</div>
+				<Provider>
+					<Navbar />
+					<div className='max-w-4xl w-[92vw] sm:w-[90vw] mx-auto'>
+						<StickySocial />
+						{children}
+						<Footer />
+						<Analytics />
+					</div>
+				</Provider>
 			</body>
 		</html>
 	)
