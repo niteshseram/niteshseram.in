ad965325dd9bf6ecfa045c802650c2f519c5fbf3
Nitesh Seram <niteshseram@gmail.com>
2023-05-29 22:39:41 +0530

    remove console.log

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -8,7 +8,6 @@ const nowPlaying = async (
 	res: NextApiResponse<NowPlayingSong>
 ) => {
 	const response = await getNowPlaying()
-  console.log(response);
 
 	if (response.status === 204 || response.status > 400) {
 		return res.status(200).json({
