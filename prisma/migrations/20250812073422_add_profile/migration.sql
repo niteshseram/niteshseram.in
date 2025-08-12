2e1eae8724b7d01b620e03b0ad9e657271268413
Nitesh Seram <niteshseram@gmail.com>
2025-08-12 13:04:35 +0530

    Add profile

---
+++ prisma/migrations/20250812073422_add_profile/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_email_key" ON "Profile"("email");
