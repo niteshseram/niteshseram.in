7f954fc9cbb46237e6fff5d7ff375a1e5d24fa31
Nitesh Seram <niteshseram@gmail.com>
2026-04-17 19:41:49 +0530

    feat: update project structure and add new components
    - Added global styles and layout for the application in `src/app/globals.css` and `src/app/layout.tsx`.
    - Implemented a new `Home` page in `src/app/page.tsx` with a responsive design.
    - Created reusable `Anchor` component in `src/components/ui/anchor.tsx` for navigation links.
    - Introduced theming utilities in `src/components/ui/theme.tsx`.
    - Added font management in `src/lib/fonts.ts` to handle custom fonts.
    - Enhanced utility functions in `src/lib/utils.ts` for class name management.
    - Updated TypeScript configuration in `tsconfig.json` to reflect new paths.
    - Removed old global CSS file and unnecessary files.
    - Added VSCode settings for consistent formatting.
    - Updated dependencies in `package.json` and `pnpm-lock.yaml` to the latest versions.

---
+++ src/components/ui/anchor.tsx
@@ -0,0 +1,118 @@
+'use client';
+
+import url from 'url';
+
+import { cva } from 'class-variance-authority';
+import Link, { type LinkProps } from 'next/link';
+import type React from 'react';
+import type { HTMLAttributeAnchorTarget } from 'react';
+
+import { cn } from '@/lib/utils';
+
+import {
+  themeOutlineElement_FocusVisible,
+  themeOutlineFocusedColor,
+} from './theme';
+
+export type Props<RouteType> = LinkProps &
+  Readonly<{
+    children?: React.ReactNode;
+    className?: string;
+    rel?: string;
+    target?: HTMLAttributeAnchorTarget;
+    variant?: AnchorVariant;
+    weight?: AnchorWeight;
+    scroll?: LinkProps<RouteType>['scroll'];
+    ref?: React.Ref<HTMLAnchorElement>;
+  }>;
+
+export function Anchor<RouteType>({
+  children,
+  className: classNameProp,
+  href,
+  onClick,
+  ref,
+  rel: relProp,
+  target: targetProp,
+  variant,
+  weight,
+  ...props
+}: Props<RouteType>) {
+  const isExternalURL =
+    typeof href === 'string' && /^(https?:|mailto:|tel:)/.test(href);
+
+  const rel = relProp ?? (isExternalURL ? 'noreferrer noopener' : undefined);
+  const className = anchorVariants({
+    className: cn(themeOutlineElement_FocusVisible, classNameProp),
+    variant,
+    weight,
+  });
+
+  const target = targetProp ?? (isExternalURL ? '_blank' : undefined);
+
+  if (isExternalURL) {
+    const finalHrefString = url.format(href);
+
+    return (
+      <a
+        ref={ref}
+        className={className}
+        href={finalHrefString}
+        rel={rel}
+        target={target}
+        onClick={onClick}
+        {...props}
+      >
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <Link
+      ref={ref}
+      className={className}
+      href={href}
+      rel={rel}
+      target={target}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
+
+export type AnchorVariant = 'default' | 'brand' | 'unstyled';
+
+export type AnchorWeight = 'inherit' | 'medium' | 'normal';
+
+const anchorVariantClasses: Record<AnchorVariant, string> = {
+  default: cn(
+    'text-muted-foreground hover:text-foreground active:text-foreground',
+    themeOutlineFocusedColor,
+  ),
+  brand: cn(
+    'underline underline-offset-[3px] decoration-[1.5px]',
+    'text-foreground decoration-link',
+    'hover:decoration-foreground',
+  ),
+  unstyled: themeOutlineFocusedColor,
+};
+
+const anchorWeightClasses: Record<AnchorWeight, string> = {
+  inherit: '',
+  medium: 'font-medium',
+  normal: 'font-normal',
+};
+
+export const anchorVariants = cva(cn('transition-colors', 'break-words'), {
+  defaultVariants: {
+    variant: 'default',
+    weight: 'medium',
+  },
+  variants: {
+    variant: anchorVariantClasses,
+    weight: anchorWeightClasses,
+  },
+});
---
+++ src/components/ui/theme.tsx
@@ -0,0 +1,3 @@
+export const themeOutlineElement_FocusVisible =
+  'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2';
+export const themeOutlineFocusedColor = 'outline-smds-border-focused';
